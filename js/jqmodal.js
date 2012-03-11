9dbc63045d6e293e5503e5b280e8dcf5c0f1a98e
Kevin Baugh <loraxx753@gmail.com>
2012-03-11 12:40:24 -0400

    added comments, jquery ui is no longer required, and added more examples

--- js/jqmodal.js
+++ js/jqmodal.js
@@ -4,40 +4,61 @@
  * defaults:
  *
  * {
- *		  'modal'                : true,
- *		  'backgroundStyle'      : 'default',
- *		  'html'                 : '',
- *		  'name'                 : '',
- *		  'src'                  : '',
- *		  'closingSelectors'     : [],
+ *		  //Whether the background darkens or not.
+ *		  modal                : true,
+ *		  //The style of the popup window (currently either default or dark).
+ *		  backgroundStyle      : 'default',
+ *		  //The html to be entered into the popup window.
+ *		  html                 : '',
+ *		  //The name of the popup window (used for styling, selected with #popup.className)
+ *		  className                 : '',
+ *		  //The href of the html if on a different file (don't use this and html together)
+ *		  src                  : '',
+ *		  //An array of selectors that, when clicked, will close the popup window.
+ *		  closingSelectors     : [],
  * };
  *
  * Create a popup with default parameters:
  *
- *	$('#button').jqmodal({
- *			html : '<p>Populates the popup with html',
- * 			closingSelectors : ['an array of selectors that will close the windows']
+ *  //Bind a link or a button to call the popup window.
+ *	$('#open').jqmodal({
+ *			html : '<p>Populates the popup with html<p><p><button id="close">Close</button>',
+ * 			closingSelectors : ['#close']
  *		})
  *	});
  *
  *
  *
- *	Options:
  **/
 
 (function( $ ){
-
+	//I really like the revealing modual pattern way of setting stuff up, so I sort of set up the methods like that
 	var methods = {
+		/**
+		 * Default init function, called with $(selector).jqmodal();
+		 */
 		init : function( options, callback ) 
 		{
+			//Binds the button to the popup
 			$(this).click(function(e) {
+				//Makes sure there's not already a popup there
+				if($('#popup').length > 0)
+				{
+					return false;
+				}
 				e.preventDefault();
+
+				//If the background needs to be darkened
 				if(options.modal)
 				{
 					methods.modal();
+					//Adds a click-to-close function on the background.
 					methods.closingSelectors($('#popupback'));
 				}
+				//Prepends the body with the popup, and adds the desired background style to it.
 				$('body').prepend('<div id="popup" class="'+options.backgroundStyle+'"></div>');
+
+				//If a name was chosen, add that class name to the popup div.
 				if(options.className != '')
 				{
 					$('#popup').addClass(options.className);
@@ -45,30 +66,42 @@
 				$('#popup').hide();
 				
 				var inner = ''
+				if(options.html && options.src)
+				{
+					//Throws an error if both the html and src options are filled out.
+					$.error('Cannot have both html and src declerations in jqmodal, pick one or the other.');
+				}
 				if(options.html)
-				{	
+				{
 					methods.build(options.html, options.closingSelectors);
 				}
 				else if(options.src)
 				{
+					//$.load() is a little weird because it needs a div to load into. Loading into a hidden dump div allows for the calculations to put it in the middle of the screen to happen after the information is loaded into the popup div.
 					$('body').prepend('<div id="dump"></div>');
 					$('#dump').hide().load(options.src, function() {
-						methods.build($('#dump').html(), options.closingSelectors, callback);
+						var html = $('#dump').html();
 						$('#dump').remove();
+						methods.build(html, options.closingSelectors, callback);
 					});
 				}
 			});
 		},
+		/**
+		 * Populates the popup window and positions it in the middle of the screen.
+		 */
 		build : function( inner, closingSelectors, callback )
 		{
 			$('#popup').html(inner);
 
+			//gets the middle positions of the window.
 			var leftMarginMinusWidthHalfWay = ($(window).width()/2) - ($('#popup').width()/2);
 			var topMarginMinusHeightHalfWay = ($(window).height()/2) - ($('#popup').height()/2);
 
 			$('#popup').css({'left' : leftMarginMinusWidthHalfWay, 'top' : topMarginMinusHeightHalfWay});
 			$('#popup').fadeIn(300);
 
+			//If there are any closing selectors chosen, then for each closing selector, bind it to close the popup.
 			if(closingSelectors.length > 0)
 			{
 				for(i in closingSelectors)
@@ -76,37 +109,51 @@
 					methods.closingSelectors(closingSelectors[i]);
 				}
 			}
-			var tabPlace = 0;
-			var tabArray =  $('#popup').find(':tabbable');
-			$(window).keydown(function(e) {
-			   var code = e.keyCode || e.which;
-			   if (code == '9') {
-			   		if(e.shiftKey)
-			   		{
-			   			tabPlace--;
-				   		if(tabPlace == -1)
+
+			//The following try binds the tab index to the modal window, but requires jQuery UI to work.
+			try {
+				var tabPlace = 0;
+				//Little known jQuery UI selector, :tabbable. Will find anything that's able to be selected with the tab key.
+				var tabArray =  $('#popup').find(':tabbable');
+				tabArray[tabPlace].focus();
+				$(window).keydown(function(e) {
+				   var code = e.keyCode || e.which;
+				   if (code == '9') {
+				   		if(e.shiftKey)
 				   		{
-				   			tabPlace = (tabArray.length-1);
+				   			tabPlace--;
+					   		if(tabPlace == -1)
+					   		{
+					   			tabPlace = (tabArray.length-1);
+					   		}
 				   		}
-			   		}
-			   		else
-			   		{
-				   		tabPlace++;
-				   		if(tabPlace == tabArray.length)
+				   		else
 				   		{
-				   			tabPlace = 0;
-				   		}			   			
-			   		}
-			   		tabArray[tabPlace].focus();
-				   return false;
-			   }
-			});
+					   		tabPlace++;
+					   		if(tabPlace == tabArray.length)
+					   		{
+					   			tabPlace = 0;
+					   		}			   			
+				   		}
+					   	tabArray[tabPlace].focus();
+					   return false;
+				   }
+				});
+			}
+			catch(e)
+			{
+				//console.log(e.message);
+			}
+			//If there's a callback, it get's fired here.
 			if(callback)
 			{
 				callback();
 			}
 
 		},
+		/**
+		 * Binds the selected element to close the popup window.
+		 */
 		closingSelectors : function( element ) 
 		{
 			$(element).click(function() {
@@ -115,6 +162,9 @@
 				$(window).unbind('keydown');
 			});
 		},
+		/**
+		 * Makes the darkened background
+		 */
 		modal : function() 
 		{
 			width = $(document).width()+100;
@@ -127,33 +177,28 @@
 			$('#popupback').width(width);
 			$('#popupback').height(height);
 		},
-		loader : function( text )
-		{
-			methods.init({
-					modal : false,
-					backgroundStyle : 'dark',
-				}, function() {
-					$('#popup').addClass('loading_popup');
-					$('#popup').html('<span id="loading">'+text+'</span> <div class="inline" id="loading_image"><div id="bowlG"><div id="bowl_ringG"><div class="ball_holderG"><div class="ballG"></div></div></div></div></div>');	
-			});	
-		}
 	};
 
 	$.fn.jqmodal = function( method, options ) {
+		//Initial settings
 		var settings = {
 		  'modal'                : true,
 		  'backgroundStyle'      : 'default',
 		  'html'                 : '',
-		  'name'                 : '',
+		  'className'                 : '',
 		  'src'                  : '',
 		  'closingSelectors'     : [],
 		};
 
+		//Makes it so none of the methods can be called publicly.
 		var privateMethods = [
 			'modal',
+			'build',
+			'closingSelectors',
+			'init',
 		];
 
-		// Method calling logic
+		// TODO: Change this since we're not using method calling really anymore.
 		if ( methods[method] ) 
 		{
 			if($.inArray(method, privateMethods) < 0)
@@ -162,7 +207,7 @@
 			}
 			else
 			{
-				$.error( 'Method ' +  method + ' does not exist on jQuery.popup' );
+				$.error( 'Method ' +  method + ' does not exist or is private on $.jqmodal' );
 			}
 		} 
 		else if ( typeof method === 'object' || ! method ) 
@@ -182,7 +227,7 @@
 		}
 		else 
 		{
-			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+			$.error( 'Method ' +  method + ' does not exist or is private on $.jqmodal' );
 		}    
 	};
 
